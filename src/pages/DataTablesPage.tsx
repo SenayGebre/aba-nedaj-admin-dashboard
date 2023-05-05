96fcf1af105f97d54c1d6c623851d930682d0c73
nardos abate <nardosyosef123@gmail.com>
2023-05-06 09:21:58 +1100

    admin_table

--- src/pages/DataTablesPage.tsx
+++ src/pages/DataTablesPage.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { Tables } from '@app/components/tables/Tables/Tables';
-import { PageTitle } from '@app/components/common/PageTitle/PageTitle';
 
+import { PageTitle } from '@app/components/common/PageTitle/PageTitle';
+import  App  from '@app/components/table/table';
 const DataTablesPage: React.FC = () => {
   const { t } = useTranslation();
   return (
     <>
-      <PageTitle>{t('common.dataTables')}</PageTitle>
-      <Tables />
+      <PageTitle>{t('common.tables')}</PageTitle>
+      <App />
     </>
   );
 };
